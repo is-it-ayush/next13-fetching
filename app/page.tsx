ea50fefc6a84af1e14ba4938ff2c3adc65963c52
Ayush Gupta <ayush.keshav2004@gmail.com>
2022-10-31 12:56:45 +0530

    changed api

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
-export default async function Home({ name }: { name: string }) {
-  const res = await fetch("http://localhost:3000/api/hello");
-  const data = await res.json();
+import { Benchmark } from "../components/benchmark";
 
+const revalidate = 10;
+
+export default async function Home() {
+  // SSR Fetching
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const data = await res.json();
   console.log(`data`, data);
 
   return (
     <div className="">
+      <div>Page is Created at: {new Date().getTime()}</div>
       <main className="">
-        <h1 className="">Hi, This is route `/`. Your name is {name}</h1>
+        <Benchmark>NextJS13 Perf</Benchmark>
+        <h1 className="">Hi, This is route `/`.</h1>
+        <pre>
+          <code>{JSON.stringify(data, null, 2)}</code>
+        </pre>
       </main>
     </div>
   );
